bf9d8387bebaa1a1a7dc844e211a2b92a8da8b23
kakudo415 <kakudo415@gmail.com>
2018-12-12 15:56:37 +0900

    format json fix

--- src/api/api.go
+++ src/api/api.go
@@ -24,17 +24,10 @@ func JSON(c *gin.Context) {
 
 func serve(c *gin.Context, infos []string) {
 	c.Header("Content-Type", "application/json; charset=utf-8")
-	if len(infos) == 0 {
-		c.String(200, `{"body":{}}`)
-		return
-	}
 	data := `{"body":{` + strings.Join(infos, `,`) + `}}`
 	if c.Query("format") == "true" {
 		var buf bytes.Buffer
-		if json.Indent(&buf, []byte(data), "", "  ") != nil {
-			c.String(200, `{"body":{}}`)
-			return
-		}
+		json.Indent(&buf, []byte(data), "", "  ")
 		c.String(200, buf.String())
 	} else {
 		c.String(200, data)

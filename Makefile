06062a950729057d3412392e93b8d472fc8dc97d
kakudo415 <kakudo415@gmail.com>
2018-12-13 11:11:52 +0900

    Initial commit

--- Makefile
+++
@@ -1,5 +0,0 @@
-setup:
-	go get github.com/gin-gonic/gin
-	go get github.com/garyburd/redigo/redis
-	go get github.com/google/uuid
-	go get github.com/mmcdole/gofeed
--- src/main.go
+++ src/main.go
@@ -1,23 +1,3 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-
-	"./api"
-	"./websub"
-)
-
-func main() {
-	app := gin.New()
-
-	// Subscriber
-	app.GET("/subscriber", websub.Subscriber)
-	app.POST("/subscriber", websub.Receiver)
-	// API
-	app.GET("/", api.Top)
-	app.GET("/xml/:uuid", api.XML)
-	app.GET("/json/:uuid", api.JSON)
-
-	// Run on $PORT
-	app.Run()
-}
+func main() {}

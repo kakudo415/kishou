62e924451a9476cd9163148e02b241e60b44ad68
Kakudo <kakudo415@gmail.com>
2019-01-31 03:13:26 +0900

    Error handler = 404

--- src/main.go
+++ src/main.go
@@ -12,9 +12,16 @@ import (
 func main() {
 	app := echo.New()
 	app.GET("/", api.Top)
+	// JSON & XML API
 	app.GET("/json/:uuid", api.JSON)
 	app.GET("/xml/:uuid", api.XML)
+	// Subscriber
 	app.GET("/sub", websub.Sub)
 	app.POST("/sub", websub.Sub)
+	// Error = 404
+	app.HTTPErrorHandler = func(e error, c echo.Context) {
+		c.NoContent(404)
+	}
+	// Run on $PORT
 	app.Start(":" + os.Getenv("PORT"))
 }
